74aa6e720bfa350ead0ef22a622b6923b41d0b45
Oscar Spencer <oscar@grain-lang.org>
2021-09-09 22:14:36 -0400

    Better storage key and string return

--- __tests__/main.spec.js
+++ __tests__/main.spec.js
@@ -1,34 +1,34 @@
-const path = require('path');
-const { Runner } = require('near-runner');
+const path = require("path");
+const { Runner } = require("near-runner");
 
 const runner = Runner.create(async ({ root }) => ({
-    contract:  await root.createAndDeploy(
-      'grain-near-simple-counter',
-      path.join(__dirname, '..', 'index.gr.wasm'))
-    }));
+  contract: await root.createAndDeploy(
+    "grain-near-simple-counter",
+    path.join(__dirname, "..", "index.gr.wasm")
+  ),
+}));
 
-describe('counter contract', () => {
+describe("counter contract", () => {
   jest.setTimeout(60_000);
 
-  test.concurrent('increments & decrements', async () => {
+  test.concurrent("increments & decrements", async () => {
     await runner.run(async ({ root, contract }) => {
-      await root.call(contract, 'increment', {});
-      await root.call(contract, 'increment', {});
-      await root.call(contract, 'decrement', {});
-      await root.call(contract, 'increment', {});
+      await root.call(contract, "increment", {});
+      await root.call(contract, "increment", {});
+      await root.call(contract, "decrement", {});
+      await root.call(contract, "increment", {});
 
-      await root.call(contract, 'increment', {});
+      await root.call(contract, "increment", {});
 
-      await root.call(contract, 'increment', {});
+      await root.call(contract, "increment", {});
 
-      console.log(await contract.view('getCounter'))
+      console.log(await contract.view("getCounter"));
       /* Should print:
          ContractState {
-           data: Map(1) { '\x00\x00\x00\x00\x00\x00\x00\x00' => <Buffer 01 00 00 00> }
+           data: Map(1) { 'counter' => <Buffer 04 00 00 00> }
          }
       */
       console.log(await contract.viewState());
     });
   });
-
 });
