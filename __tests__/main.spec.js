31232690d73b13bcf2b6f70c89ee92cd16ddcebe
Oscar Spencer <oscar@grain-lang.org>
2021-09-13 14:15:53 -0400

    tiny near sdk

--- __tests__/main.spec.js
+++ __tests__/main.spec.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const { Runner } = require("near-runner");
 
-const runner = Runner.create(async ({ root }) => ({
+const runner = Runner.create({ network: "testnet" }, async ({ root }) => ({
   contract: await root.createAndDeploy(
     "grain-near-simple-counter",
     path.join(__dirname, "..", "index.gr.wasm")
@@ -22,13 +22,13 @@ describe("counter contract", () => {
 
       await root.call(contract, "increment", {});
 
-      console.log(await contract.view("getCounter"));
+      expect(await contract.view("getCounter")).toEqual(4);
       /* Should print:
          ContractState {
            data: Map(1) { 'counter' => <Buffer 04 00 00 00> }
          }
       */
-      console.log(await contract.viewState());
+      // console.log(await contract.viewState());
     });
   });
 });
