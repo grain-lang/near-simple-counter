185f9390144c1f69b7d98c509870b89ee7ee3cb3
Blaine Bublitz <blaine.bublitz@gmail.com>
2021-09-13 19:09:24 -0700

    feat: Update for Grain 0.4 (#2)
    feat: Add near-runner testing
    
    Co-authored-by: Oscar Spencer <oscar@grain-lang.org>

---
+++ __tests__/main.spec.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const { Runner } = require("near-runner");
+
+const runner = Runner.create(async ({ root }) => ({
+  contract: await root.createAndDeploy(
+    "grain-near-simple-counter",
+    path.join(__dirname, "..", "index.gr.wasm")
+  ),
+}));
+
+describe("counter contract", () => {
+  jest.setTimeout(60_000);
+
+  test.concurrent("increments & decrements", async () => {
+    await runner.run(async ({ root, contract }) => {
+      await root.call(contract, "increment", {});
+      await root.call(contract, "increment", {});
+      await root.call(contract, "decrement", {});
+      await root.call(contract, "increment", {});
+
+      await root.call(contract, "increment", {});
+
+      await root.call(contract, "increment", {});
+
+      expect(await contract.view("getCounter")).toEqual(4);
+    });
+  });
+});
